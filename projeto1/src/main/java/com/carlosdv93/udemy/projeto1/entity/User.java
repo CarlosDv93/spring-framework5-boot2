65a7e0b2cce13e19355c3ea34c9f3b39fd86ffce
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-01-21 10:30:25 -0200

    Primeiro Commit

---
+++ projeto1/src/main/java/com/carlosdv93/udemy/projeto1/entity/User.java
@@ -0,0 +1,36 @@
+package com.carlosdv93.udemy.projeto1.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String nome;
+	private String email;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}
