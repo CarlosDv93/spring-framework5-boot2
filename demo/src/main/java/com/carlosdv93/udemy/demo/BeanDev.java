6c42faa05ac5abd8b5234bfe552aea28931dc187
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-01-22 14:21:05 -0200

    Retirando imports não usados

--- demo/src/main/java/com/carlosdv93/udemy/demo/BeanDev.java
+++ demo/src/main/java/com/carlosdv93/udemy/demo/BeanDev.java
@@ -1,8 +1,5 @@
 package com.carlosdv93.udemy.demo;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
 public class BeanDev {
 
 	public BeanDev() {
