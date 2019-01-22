7e35f199b5528cc971a6d9ab4842aab362e9a9e1
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-01-22 15:18:58 -0200

    Aula 31 - Application.properties

---
+++ aulas-secao5/src/main/java/com/carlosdv93/udemy/aulas/AulasSecao5Application.java
@@ -0,0 +1,14 @@
+package com.carlosdv93.udemy.aulas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasSecao5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasSecao5Application.class, args);
+	}
+
+}
+
