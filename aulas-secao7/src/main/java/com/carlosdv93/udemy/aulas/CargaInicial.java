671551d90a75ffaea20a03fc53a3aec60e1a4959
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-01-24 16:07:42 -0200

    Aula 46 - Persistence

--- aulas-secao7/src/main/java/com/carlosdv93/udemy/aulas/CargaInicial.java
+++ aulas-secao7/src/main/java/com/carlosdv93/udemy/aulas/CargaInicial.java
@@ -1,47 +1,50 @@
 package com.carlosdv93.udemy.aulas;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 
 import com.carlosdv93.udemy.aulas.entity.Role;
+import com.carlosdv93.udemy.aulas.entity.User;
 import com.carlosdv93.udemy.aulas.enums.StatusRole;
 import com.carlosdv93.udemy.aulas.repository.RoleRepository;
+import com.carlosdv93.udemy.aulas.repository.UserRepository;
 
 @Component
 public class CargaInicial implements ApplicationListener<ContextRefreshedEvent> {
 
 	@Autowired
-	private RoleRepository roleRP;
+	private UserRepository userRP;
 
+	/*
+	//Pq foi add no User o CascadeType.PERSIST no @manyToOne não é necessário ter o RP injetado
+	@Autowired
+	private RoleRepository roleRP;
+	*/
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
 		
-		for(int i = 0; i< 100; i++) {
-			this.saveRole("Admin", StatusRole.ATIVO);
-		}
+		Role role = new Role("Admin", StatusRole.ATIVO);
+		/*roleRP.save(role);*/
 		
-		for(int i = 0; i< 100; i++) {
-			this.saveRole("User", StatusRole.INATIVO);
-		}
+		User user = new User("carlos", "carlos@carlos.com");
+		user.setRole(role);
 		
-		PageRequest page = PageRequest.of(1, 10);
+		userRP.save(user);
 		
-		Page<Role> roles = this.roleRP.findAll(page);
+		List<User> userR = this.userRP.findAll();
 		
-		for(Role role : roles) {
-			System.out.println(role.getName());
+		for(User user2 :userR) {
+			Role role2 = user2.getRole();
+			
+			System.out.println(role2.getName());
 		}
 
 	}
 	
-	public void saveRole(String nome, StatusRole status) {
-		Role role = new Role(nome, status);
-		
-		roleRP.save(role);
-	}
+
 
 }
