769f7d0ceb1ceae0d7902d706bea80335731c5b3
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-01-25 09:41:51 -0200

    Aula 55 - Criar um CRUD simples e utilizar o Postman

--- aulas-secao8/src/main/java/com/carlosdv93/udemy/aulas/controller/UserController.java
+++ aulas-secao8/src/main/java/com/carlosdv93/udemy/aulas/controller/UserController.java
@@ -3,6 +3,8 @@ package com.carlosdv93.udemy.aulas.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +23,22 @@ public class UserController {
 	public List<User> getAllUsers() {
 		return userRP.findAll();
 	}
+	
+	@RequestMapping(value="", method = RequestMethod.POST)
+	public User saveUser(@RequestBody User user) {
+		return userRP.save(user);
+	}
+	
+	@RequestMapping(value="", method=RequestMethod.PUT)
+	public User editUser(@RequestBody User user) {
+		return userRP.save(user);
+	}
+	
+	@RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    public void findAll(@PathVariable Long id){
+        userRP.deleteById(id);
+    }
+
+	
 	
 }
